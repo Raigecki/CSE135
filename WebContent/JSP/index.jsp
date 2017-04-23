b8f40e2a02c2d37acca78d64ac74186937b58118
alephv0id <generalstarshade@gmail.com>
2017-04-22 18:06:19 -0700

    Started controller and jsp stuff

--- WebContent/JSP/index.jsp
+++ WebContent/JSP/index.jsp
@@ -6,6 +6,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script src="Javascripts/main.js"></script>
 <title>Sign Up</title>
 </head>
 <body>
@@ -23,6 +24,24 @@
 			<button type="button" class="btn" id="btn_submit">
 				Submit
 			</button>
+		</div>
+		
+		
+		<div>
+		
+			Username: <input type="text" id="input_userName"> </br>
+			Age: <input type="text" id="input_age"> </br>
+			State: 
+			<select>
+  				<option value="1">Hawaii</option>
+  				<option value="2">California</option>
+  			</select> </br>
+			<select>
+  				<option value="0">Owner</option>
+  				<option value="1">Customer</option>
+  			</select> </br>
+			<button type="button" class="btn" id="btn_signUp">Submit</button>
+			
 		</div>
 	</div>
 
--- src/ApplicationControls/Controller_SignUp.java
+++ src/ApplicationControls/Controller_SignUp.java
@@ -1,9 +1,42 @@
 package ApplicationControls;
+import java.sql.*;
 
 public class Controller_SignUp {
 
 	public Boolean CheckUser(String userName) {
 		
 		return false;
+	}
+
+	
+	public boolean createUser(String userInfo[]) throws SQLException, ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+		
+		// open a connection to the database
+		Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/jdbc-examples?" + "user=cse135&password=sheevspin");
+		
+		String name = userInfo[0];
+		int age = Integer.parseInt(userInfo[1]);
+		int state = Integer.parseInt(userInfo[2]);
+		int role = Integer.parseInt(userInfo[3]);
+		
+		PreparedStatement checkUserStmt = conn.prepareStatement("SELECT name FROM user WHERE name = ?");
+		
+		checkUserStmt.setString(1, name);
+		ResultSet rset = checkUserStmt.executeQuery();
+		if (rset.next()) {
+			return false;
+		}
+		else {
+			PreparedStatement createUserStmt = conn.prepareStatement("INSERT INTO user (name, age, state, role) " +
+																	 "VALUES (?, ?, ?, ?)");
+			createUserStmt.setString(1, name);
+			createUserStmt.setLong(2, age);
+			createUserStmt.setLong(3, state);
+			createUserStmt.setLong(4, role);
+			int rmsg = checkUserStmt.executeUpdate();
+			return true;
+			
+		}
 	}
 }
